79c43fed6e14723ca1cc63a125d39537d05487ae
jlaluces123 <jlalucescareer123@gmail.com>
2019-01-18 09:01:35 -0800

    Ex2: Ex2 Done

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -4,13 +4,37 @@
 #include "hashtable.h"
 #include "ex2.h"
 
+/*
+  We can hash each ticket such that the starting location is the key 
+  and the destination is the value. 
+  Then, when constructing the entire route, 
+  the ith location in the route can be found by checking the 
+  hash table for the i-1th location.
+*/
 char **reconstruct_trip(Ticket **tickets, int length)
 {
   HashTable *ht = create_hash_table(16);
   char **route = malloc(length * sizeof(char *));
 
-  // YOUR CODE HERE
+  // 1. First we want to check if starting dest is NONE, so we can set first part of our route to the destination
+  for (int i = 0; i < length; i++) {
+    if ( strcmp(tickets[i]->source, "NONE") == 0 ) {
+      route[0] = tickets[i]->destination; // --> EX: Since None: { PDX -> }
+    } // 1a. As stated in hints: we can hash the ticket after
+    hash_table_insert(ht, tickets[i]->source, tickets[i]->destination); // --> ht || NONE == Key || PDX == Value
+  }
 
+  // 2. We need to make the rest of the route (done by method given in hint)
+  for (int i = 1; i < length; i++) { // --> We need to start at 1 here because there is no -1 pos in route
+    if (hash_table_retrieve(ht, route[i - 1]) != NULL) { // --> Meaning we found a match in keys
+      route[i] = hash_table_retrieve(ht, route[i - 1]); // --> retrieve will return the value at that key
+    } else { // --> No matching Key
+      route[i] = "NONE"; // --> end of trip
+    }
+  }
+
+  // 3. Don't forget to free ht after
+  destroy_hash_table(ht);
   return route;
 }
 
